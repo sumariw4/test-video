e7c16e029e1047364a4c9a09f2171f7cabfaad7a
pandaicoding <pandaicoding@gmail.com>
2024-01-09 15:06:17 +0700

    Update v2

--- main.js
+++ main.js
@@ -1,94 +1,84 @@
-const videoSourceURL =
-  "https://file-examples.com/storage/fe42f94178659ccad957366/2017/04/file_example_MP4_1280_10MG.mp4";
+const videoSourceURL = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
-function disableRightClick(event) {
-  event.preventDefault();
-}
-
-function disableMobileRecording() {
+function source() {
   const videoPlayer = document.getElementById("secureVideoPlayer");
 
   // Set the video source with HTTPS
   videoPlayer.src = videoSourceURL;
 
-  // Add event listener to disable right-click on the video player
-  videoPlayer.addEventListener("contextmenu", disableRightClick);
+  // Disable Audio
+  videoPlayer.muted = true;
 
-  // Add event listener to handle the play event
-  videoPlayer.addEventListener("play", () => {
-    // Check if the video is playing in fullscreen
-    if (document.fullscreenElement || document.webkitFullscreenElement) {
-      // Stop the video playback if fullscreen is detected
-      videoPlayer.pause();
-      // You can also display a warning message to the user
-    }
-  });
-
-  // Add event listener to handle screen capture attempts
-  document.addEventListener("visibilitychange", () => {
-    if (document.visibilityState === "hidden") {
-      // Detecting if the screen is black (e.g., screenshot attempt)
-      const canvas = document.createElement("canvas");
-      const context = canvas.getContext("2d");
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-
-      context.fillStyle = "black";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height).data;
-
-      // Check if all pixels are black (R = 0, G = 0, B = 0, A = 255)
-      const isBlackScreen = imageData.every((value, index) => ((index + 1) % 4 === 0 ? value === 255 : value === 0));
-
-      if (isBlackScreen) {
-        // Stop the video playback if a black screen is detected
-        videoPlayer.pause();
-        // You can also display a warning message to the user
-      }
-    }
-  });
-
-  //   Disable Screenshot Using Javascript
-  document.addEventListener("keydown", function (event) {
-    navigator.clipboard.writeText("Screenshot Disabled");
-    if (event.key == "PrintScreen") {
-      event.preventDefault();
-      alert("Screenshot Disabled");
-    }
-  });
-
-  document.addEventListener("keyup", function (event) {
-    if (event.key == "PrintScreen") {
-      event.preventDefault();
-      alert("Screenshot Disabled");
-    }
-  });
+  // Auto Play Video
+  videoPlayer.autoplay = true;
 }
 
-document.addEventListener("DOMContentLoaded", disableMobileRecording);
+document.addEventListener("DOMContentLoaded", source);
 
-document.addEventListener("keyup", function (e) {
-  var keyCode = e.keyCode ? e.keyCode : e.which;
-  if (keyCode == 44) {
-    stopPrntScr();
+// Disable Screen Capture
+document.addEventListener("securitypolicyviolation", function () {
+  console.log("gotpointercapture");
+});
+
+// Disable PrintScreen
+document.addEventListener("keydown", function (event) {
+  if (event.key == "PrintScreen") {
+    event.preventDefault();
+    alert("Screenshot Disabled");
   }
 });
-function stopPrntScr() {
-  var inpFld = document.createElement("input");
-  inpFld.setAttribute("value", ".");
-  inpFld.setAttribute("width", "0");
-  inpFld.style.height = "0px";
-  inpFld.style.width = "0px";
-  inpFld.style.border = "0px";
-  document.body.appendChild(inpFld);
-  inpFld.select();
-  document.execCommand("copy");
-  inpFld.remove(inpFld);
-}
-function AccessClipboardData() {
-  try {
-    window.clipboardData.setData("text", "Access   Restricted");
-  } catch (err) {}
-}
-setInterval("AccessClipboardData()", 300);
+
+document.addEventListener("keyup", function (event) {
+  if (event.key == "PrintScreen") {
+    event.preventDefault();
+    alert("Screenshot Disabled");
+  }
+});
+
+//Disable inspect element,
+document.addEventListener("keydown", function (event) {
+  // F12
+  if (event.key == "F12") {
+    event.preventDefault();
+    alert("Inspect Element Disabled");
+  }
+  // Ctrl+Shift+I
+  if (event.ctrlKey && event.shiftKey && event.key == "I") {
+    event.preventDefault();
+    alert("Inspect Element Disabled #I");
+  }
+  //  Ctrl+Shift+J
+  if (event.ctrlKey && event.shiftKey && event.key == "J") {
+    event.preventDefault();
+    alert("Inspect Element Disabled #J");
+  }
+  // Ctrl+Shift+U
+  if (event.ctrlKey && event.shiftKey && event.key == "U") {
+    event.preventDefault();
+    alert("Inspect Element Disabled #U");
+  }
+  if (event.ctrlKey && event.shiftKey && event.key == "C") {
+    event.preventDefault();
+    alert("Inspect Element Disabled #C");
+  }
+
+  if (event.ctrlKey && event.shiftKey && event.key == "S") {
+    event.preventDefault();
+    alert("Inspect Element Disabled #S");
+  }
+});
+
+document.addEventListener(
+  "contextmenu",
+  function (e) {
+    e.preventDefault();
+    alert("Screen Capture Disabled");
+  },
+  false
+);
+
+// Disable screen capture mobile
+document.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+  alert("Screen Capture Disabled");
+});
