8827c2fc3cb51144140ac3fd8169999ecf4e3434
pandaicoding <pandaicoding@gmail.com>
2024-01-09 12:00:17 +0700

    Update Prevent Screen Shoot

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const videoSourceURL = "https://samplelib.com/lib/preview/mp4/sample-20s.mp4";
+const videoSourceURL =
+  "https://file-examples.com/storage/fe42f94178659ccad957366/2017/04/file_example_MP4_1280_10MG.mp4";
 
 function disableRightClick(event) {
   event.preventDefault();
@@ -66,3 +67,28 @@ function disableMobileRecording() {
 }
 
 document.addEventListener("DOMContentLoaded", disableMobileRecording);
+
+document.addEventListener("keyup", function (e) {
+  var keyCode = e.keyCode ? e.keyCode : e.which;
+  if (keyCode == 44) {
+    stopPrntScr();
+  }
+});
+function stopPrntScr() {
+  var inpFld = document.createElement("input");
+  inpFld.setAttribute("value", ".");
+  inpFld.setAttribute("width", "0");
+  inpFld.style.height = "0px";
+  inpFld.style.width = "0px";
+  inpFld.style.border = "0px";
+  document.body.appendChild(inpFld);
+  inpFld.select();
+  document.execCommand("copy");
+  inpFld.remove(inpFld);
+}
+function AccessClipboardData() {
+  try {
+    window.clipboardData.setData("text", "Access   Restricted");
+  } catch (err) {}
+}
+setInterval("AccessClipboardData()", 300);
